592ff78fb13104236ee3565c7f03fc142d2586a3
Allison van Gemert <avangemert@Allisons-MacBook-Pro.local>
2018-04-30 14:59:13 -0500

    python-practice

--- Coding/Week3-Python/main.py
+++ Coding/Week3-Python/main.py
@@ -6,7 +6,8 @@ import datetime
 
 # Import the employee_data1.csv and employee_data2.csv
 
-filepath = os.path.join("/Users/avangemert/Downloads/employee_data1.csv")
+filepath = os.path.join("PyBoss", "employee_data1.csv")
+filepath = os.path.join("PyBoss", "employee_data2.csv")
 
 converted_employee_data = []
 
@@ -30,7 +31,87 @@ with open(filepath) as csvfile:
 
         # The SSN should only show the last four digits.
         ssn_mask = ssn[-4:].rjust(len(ssn), "*")
-        
+
+        # Reference US state abbreviations dictionary.
+        us_state_abbrev = {
+            'Alabama': 'AL',
+            'Alaska': 'AK',
+            'Arizona': 'AZ',
+            'Arkansas': 'AR',
+            'California': 'CA',
+            'Colorado': 'CO',
+            'Connecticut': 'CT',
+            'Delaware': 'DE',
+            'Florida': 'FL',
+            'Georgia': 'GA',
+            'Hawaii': 'HI',
+            'Idaho': 'ID',
+            'Illinois': 'IL',
+            'Indiana': 'IN',
+            'Iowa': 'IA',
+            'Kansas': 'KS',
+            'Kentucky': 'KY',
+            'Louisiana': 'LA',
+            'Maine': 'ME',
+            'Maryland': 'MD',
+            'Massachusetts': 'MA',
+            'Michigan': 'MI',
+            'Minnesota': 'MN',
+            'Mississippi': 'MS',
+            'Missouri': 'MO',
+            'Montana': 'MT',
+            'Nebraska': 'NE',
+            'Nevada': 'NV',
+            'New Hampshire': 'NH',
+            'New Jersey': 'NJ',
+            'New Mexico': 'NM',
+            'New York': 'NY',
+            'North Carolina': 'NC',
+            'North Dakota': 'ND',
+            'Ohio': 'OH',
+            'Oklahoma': 'OK',
+            'Oregon': 'OR',
+            'Pennsylvania': 'PA',
+            'Rhode Island': 'RI',
+            'South Carolina': 'SC',
+            'South Dakota': 'SD',
+            'Tennessee': 'TN',
+            'Texas': 'TX',
+            'Utah': 'UT',
+            'Vermont': 'VT',
+            'Virginia': 'VA',
+            'Washington': 'WA',
+            'West Virginia': 'WV',
+            'Wisconsin': 'WI',
+            'Wyoming': 'WY',
+        }    
+
+        # The state data should be rewritten as a two-letter abbreviation.
+        state_abbrev = [us_state_abbrev[state]]
+
+        # Append data to converted_employee_data field
+        converted_employee_data.append(
+                {
+                    "emp_id": emp_id,
+                    "first_name": first_name,
+                    "last_name" : last_name,
+                    "dob_mdy": dob_mdy,
+                    "ssn_mask": ssn_mask,
+                    "us_state_abbrev": state_abbrev   
+                }
+        )
+
+
+        # Grab the filename from the original path
+        _, filename = os.path.split(filepath)  
+
+        # Write updated data to csv file
+        csvpath = os.path.join("output", filename)
+        with open(csvpath, "w") as csvfile:
+            fieldnames = ["emp_id", "first_name", "last_name", "dob_mdy", "ssn_mask", "us_state_abbrev"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(converted_employee_data)
 
         
              
