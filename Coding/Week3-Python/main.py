8d2956ac917e40a3059850010d565d0ca437c18b
Allison van Gemert <avangemert@Allisons-MacBook-Pro.local>
2018-04-30 10:26:52 -0500

    python-challenge

--- Coding/Week3-Python/main.py
+++ Coding/Week3-Python/main.py
@@ -2,10 +2,13 @@
 
 import os
 import csv
+import datetime
 
 # Import the employee_data1.csv and employee_data2.csv
 
-filepath = os.path.join("PyBoss", "employee_data1.csv")
+filepath = os.path.join("/Users/avangemert/Downloads/employee_data1.csv")
+
+converted_employee_data = []
 
 # Read data into dictionary
 
@@ -17,3 +20,19 @@ with open(filepath) as csvfile:
         dob = row["DOB"]
         ssn = row["SSN"]
         state = row["State"]
+       
+       # The Name column should be split into separate First Name and Last Name columns.
+        first_name = name.split()[0]
+        last_name = name.split()[1]
+       
+       # The DOB should be reformatted as MM/DD/YYYY
+        dob_mdy = datetime.datetime.strptime(dob, '%Y-%m-%d').strftime('%m/%d/%y')
+
+        # The SSN should only show the last four digits.
+        ssn_mask = ssn[-4:].rjust(len(ssn), "*")
+        
+
+        
+             
+        
+        
